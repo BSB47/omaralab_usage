40749618d3cd865c8947bb3fc4abda8d4bc21d70
Bombshell BIlly <yfu218@aucklanduni.ac.nz>
2026-05-05 16:13:49 +1000

    layout back to wide

--- parse.py
+++ parse.py
@@ -149,12 +149,15 @@ def plot(
 
 if __name__ == "__main__":
 
-    st.set_page_config(layout="centered")
+    st.set_page_config(layout="wide")
     st.title("Omara lab supercomputer usage", text_alignment="center")
     login_placeholder = st.empty()
     threshold = st.sidebar.slider("Hide slices below %", 0, 20, 5)
 
-    user_input = st.text_input("Enter Password", type="password", width=300)
+    col1, col2, col3 = st.columns([1, 1, 1])
+
+    with col2:
+        user_input = st.text_input("Enter Password", type="password", width=300)
     if user_input == st.secrets["password"]:
         st.success("Access granted")
         today = datetime.now().strftime("%Y-%m-%d")
