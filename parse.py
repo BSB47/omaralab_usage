9ff166022fcc860ab2cca5f1880d0b5381b1cd38
Bombshell BIlly <yfu218@aucklanduni.ac.nz>
2026-05-14 15:19:27 +1000

    markdown new lines work like this?

--- parse.py
+++ parse.py
@@ -223,7 +223,6 @@ if __name__ == "__main__":
     with mid_col:
         st.image(buf.getvalue(), use_container_width=True)
         st.markdown(
-            f"Authors: Frank, Emily,\
-            Rendering data from {date} (most recent available)"
+            f"Authors: Frank, Emily,  \nRendering data from {date} (most recent available)"
         )
     st.stop()
